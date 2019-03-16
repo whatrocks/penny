9b1529ab37724dcbb576100403f277c59fbb67ff
Charlie Harrington <charles.harrington@gmail.com>
2019-03-16 08:30:04 -0700

    render a list of features

---
+++ src/components/FeatureList.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul>
+    <li v-for="feature in features" :key="feature.key">{{feature.text}}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "FeatureList",
+  data: function() {
+    return {
+      features: [
+          { key: 0, text: "I love you" }, 
+          { key: 1, text: "so Much!" },
+          { key: 2, text: "This is such a great feature iwhat do tyou !" }
+        ]
+    };
+  },
+};
+</script>
+<style scoped lang="scss">
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  margin: 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>🕵️‍♀️ Penny</h1>
+    <p>Penny is your friendly neighborhood HTTP traffic inspector app.</p>
+    <h3>Features</h3>
+    <FeatureList />
+    <p>Created by
+      <a href="https://www.charlieharrington.com">Charlie Harrington</a>.
+    </p>
   </div>
 </template>
+<script>
+import FeatureList from "@/components/FeatureList.vue"
+export default {
+  name: 'about',
+  components: {
+    FeatureList
+  }
+}
+</script>
